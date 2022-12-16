05b9c9e2dfa6a9e90292e6cf35f728c72517adc8
Jungwon423 <jungwon423@naver.com>
2022-12-17 00:13:10 +0900

    Initial commit

---
+++ src/test/java/HotDeal/HotDeal/HotDealApplicationTests.java
@@ -0,0 +1,13 @@
+package HotDeal.HotDeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

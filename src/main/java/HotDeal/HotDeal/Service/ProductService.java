3ee9bd8fb818a92424cdcc5f205a2e264d458802
oreo <ohsimon77@naver.com>
2022-12-22 01:05:51 +0900

    Feat : 연습용 Product2 도메인/컨트롤러/서비스/ 레포지토리 추가

---
+++ src/main/java/HotDeal/HotDeal/Service/ProductService.java
@@ -0,0 +1,30 @@
+package HotDeal.HotDeal.Service;
+
+import HotDeal.HotDeal.Domain.Product;
+import HotDeal.HotDeal.Domain.Product2;
+import HotDeal.HotDeal.Repository.ProductsRepository2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductsRepository2 productsRepository2;
+
+    public List<Product2> findList(){
+        return productsRepository2.findByName("namu");
+    }
+
+    public List<Product2> findList2(){
+        return productsRepository2.findAll();
+    }
+    public void insertTest(String productId) {
+        Product2 product2 = new Product2();
+        product2.setName(productId);
+
+        productsRepository2.insert(product2);
+    }
+}

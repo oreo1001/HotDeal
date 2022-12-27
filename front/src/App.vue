a30bac6a411b64684db549ac5bd1387a0a01a7cf
Jungwon423 <44633262+Jungwon423@users.noreply.github.com>
2022-12-27 18:05:30 +0900

    vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -14,8 +14,6 @@ import TitleHeader from './components/TitleHeader.vue';
 import CategoryList from './components/CategoryList.vue';
 import ProductContainer from './components/ProductContainer.vue';
 import ProductList from './components/ProductList.vue';
-import axios from 'axios';
-import { provide, ref } from 'vue'
 
 export default {
   name: 'App',
@@ -28,32 +26,6 @@ export default {
 },
 
   setup() {
-
-    const categoryNames = ref([])
-    provide('categoryNames', categoryNames)
-
-    const currentCategory = ref('all')
-    provide('currentCategory', currentCategory)
-
-    const productList = ref([])
-    provide('productList', productList)
-
-
-    axios.get('/api/category/names')
-    .then(res => {
-      categoryNames.value = res.data['result']
-      console.log(categoryNames.value)
-    })
-
-    // axios.get('api/category/'+currentCategory.value+'/list')
-    // .then(res => {
-    //   productList.value = res.data['result']
-    //   console.log(productList.value)
-    // })
-
-
-
-
   },
 }
 </script>
--- front/src/components/CategoryList.vue
+++ front/src/components/CategoryList.vue
@@ -1,57 +1,59 @@
 <template>
   <v-container>
     <v-row>
-      <v-col
-        v-for="category in categoryNames"
-        :key="category['name']"
+      ddd
+      <!-- <h1>
+      {{ categoryNames }}</h1> -->
+      <!-- <v-col
+        v-for="categoryName in categoryNames"
+        :key="categoryName"
         class="align-center"
         @click="onClick"
       >
         <CategoryButton :category-name="카테고리이름" />
-      </v-col>
+        카테고리버튼
+      </v-col> -->
     </v-row>
   </v-container>
 </template>
 
   
 <script>
-import { inject, ref } from 'vue'
-import CategoryButton from './CategoryButton.vue'
+// import { ref } from 'vue'
+// import CategoryButton from './CategoryButton.vue'
 
 export default {
   name: 'CategoryList',
 
   components: {
-    CategoryButton
+    // CategoryButton
   },
 
   setup() {
 
-    currentCategory: ""
+    // currentCategory: ""
 
-    categoryNames: []
+    // categoryNames: ['패션 / 뷰티', '가구 / 생활', '가전 / 컴퓨터', '여행 / 자동차']
 
-    let counter = ref(0)
+    // let counter = ref(0)
 
-    const categoryNames = inject('categoryNames')
-
-    const onClick = (evt) => {
-      if (evt) {
-        counter.value++
-      }
-    }
+    // const onClick = (evt) => {
+    //   if (evt) {
+    //     counter.value++
+    //   }
+    // }
 
     return {
-      counter,
-      categoryNames,
-      onClick
+      // counter,
+      // categoryNames,
+      // onClick
     }
   },
   
-  async created() {
-    await this.$store.dispatch("GetProductListApi/FETCH_PRODUCTLIST_API")
-    this.currentCategory = this.$store.state.GetProductListApi.currentCategory
-    this.categoryNames = this.$store.state.GetProductListApi.categoryNames
-  }
+  // async created() {
+  //   // await this.$store.dispatch("GetProductListApi/FETCH_PRODUCTLIST_API")
+  //   // this.currentCategory = this.$store.state.GetProductListApi.currentCategory
+  //   // this.categoryNames = this.$store.state.GetProductListApi.categoryNames
+  // }
 }
 </script>

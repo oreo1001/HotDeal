ae66fe61f5b8c3a3cf6cab93700edcda5219c69e
oreo <ohsimon77@naver.com>
2022-12-30 04:47:54 +0900

    test

--- src/main/java/HotDeal/HotDeal/Controller/CategoryController.java
+++ src/main/java/HotDeal/HotDeal/Controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RequestMapping("category")
 @RequiredArgsConstructor
 public class CategoryController {
+    //test
 
     private final ProductService productService;
     private final CategoryService categoryService;
